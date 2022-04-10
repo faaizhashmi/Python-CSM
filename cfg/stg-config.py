4de78564378b4c60d6334aa07559859b8a9a6c21
Faaiz <30311213+faaizhashmi@users.noreply.github.com>
2022-04-10 06:06:43 +0500

    Add files via upload

---
+++ cfg/stg-config.py
@@ -0,0 +1,11 @@
+HOME_URL = "https://dev.commonsensemedia.org/"
+SIGNUP_URL = "https://dev.commonsensemedia.org/user/register?destination=homepage"
+
+# Browser
+BROWSER = 'Chrome'
+
+# Test user
+TEST_USER = {
+    "username": "acquirer2@mailinator.com",
+    "password": "12345678901"
+}
